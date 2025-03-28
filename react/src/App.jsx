46e4d8475637db5f0837e0e7fd3ec6432fb4df32
Rekhansika <rekhagoluguri02@gmail.com>
2025-03-28 13:28:58 +0530

    react

---
+++ react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Goluguri Rekhansika</h1>
+      <h3>goluguri.rekhansika.s89@kalvium.community</h3>
+    </>
+  )
+}
+
+export default App
